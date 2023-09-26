1fbc992e2f105003bdd0c6876db06f4315fc7a11
lavanyachadwick <lavanyarajendran16@gmail.com>
2023-09-26 13:54:02 +0530

    added code

--- com/dummy/DemoGitClass.java
+++ com/dummy/DemoGitClass.java
@@ -1,9 +1,13 @@
 package com.dummy;
 
 public class DemoGitClass {
+	public void method1() {
+		System.out.println("Method 1");
+	}
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("google-divya");
 	}
 
 }
